52afbda8997d44df8365753dfceb40d5e0a79b1c
AVEPOINT\liam.nguyen <Liam.nguyen@avepoint.com>
2022-11-14 08:31:38 +0700

    responsive mobile blog detail

--- src/containers/BlogDetail/CustomStyled.js
+++ src/containers/BlogDetail/CustomStyled.js
@@ -6,11 +6,20 @@ export const BlogDetailWrapper = styled.div`
 
   .blog-detail-back {
     padding: 20px 200px;
+
+    @media only screen and (max-width: 576px) {
+      padding: 20px 20px;
+    }
   }
 
   .blog-detail-content {
     padding: 50px 200px;
     padding-top: 0;
+
+    @media only screen and (max-width: 576px) {
+      padding: 20px 20px;
+      padding-top: 0;
+    }
 
     &-title {
       & > p:first-child {
